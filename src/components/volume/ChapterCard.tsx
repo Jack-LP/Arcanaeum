b0151706af99cd22241a4bf0ab8a88ad0cb22e70
Jack-LP <jackpaget1@gmail.com>
2023-07-01 08:34:42 +0100

    Added volume chapter display, currently displaying all books

--- src/components/volume/ChapterCard.tsx
+++ src/components/volume/ChapterCard.tsx
@@ -1,4 +1,19 @@
-const ChapterCard = () => {
-  return <div>ChapterCard</div>;
+import { Link } from 'react-router-dom';
+
+const ChapterCard = ({ chapterData, chapterTitle }) => {
+  return (
+    <div className='w-full bg-charcoal-200 p-5 rounded-md'>
+      <div className='flex flex-col gap-5'>
+        <h2 className='text-xl font-semibold'>{chapterTitle}</h2>
+        <ul className='list-inside list-disc flex flex-col items-start'>
+          {chapterData.map((item) => (
+            <li key={item.title}>
+              <Link to={'/'}>{item.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default ChapterCard;
--- src/pages/Volume.tsx
+++ src/pages/Volume.tsx
@@ -2,12 +2,14 @@ import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import DataContext from '../context/DataContext';
 import FeaturedCard from '../components/volume/FeaturedCard';
+import ChapterCard from '../components/volume/ChapterCard';
 
 const Volume = () => {
   const { libraryData } = useContext(DataContext);
   const params = useParams();
 
-  const volumeData = libraryData.filter((item) => item.volume === params.volId);
+  const volumeData = libraryData.filter((item) => item.volume === params.volId); // Filters data down to specific volume
+  const chapterData = Array.from(new Set(volumeData.map((obj) => obj.chapter))); // Returns array of chapter titles
 
   return (
     <div className='flex flex-col gap-5 w-full'>
@@ -15,6 +17,12 @@ const Volume = () => {
         params.volId
       )} - The Histories`}</h1>
       <FeaturedCard />
+      {chapterData.map((chapter) => (
+        <ChapterCard
+          chapterData={volumeData.filter((item) => item.chapter === chapter)}
+          chapterTitle={chapter}
+        />
+      ))}
     </div>
   );
 };
